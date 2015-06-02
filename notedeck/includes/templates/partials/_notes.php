79bc1118f700a6f3a394e9a3e518f4ab2bd08417
David Sudarma <david@95west.co>
2015-06-02 15:26:18 -0700

    Better bootstrapping

--- notedeck/includes/templates/partials/_notes.php
+++ notedeck/includes/templates/partials/_notes.php
@@ -6,9 +6,9 @@
 				<textarea rows="10" cols="50" placeholder="Enter your notes here..." class="form-control nd-notes-content"></textarea>
 			</div>
 				<div class="input-group">
-					<input type="email" class="form-control nd-notes-email" placeholder="Enter your email..."/>
+					<input type="email" class="form-control input-lg nd-notes-email" placeholder="Enter your email..."/>
 					<span class="input-group-btn">
-						<input type="submit" class="btn btn-default nd-notes-send" value="SEND"/>
+						<input type="submit" class="btn btn-primary btn-lg nd-notes-send" value="SEND"/>
 					</span>
 				</div>
 		</form>
