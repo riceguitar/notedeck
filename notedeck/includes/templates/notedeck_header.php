9c9d332794ff1a18513a0a0c6688b8ae428f9e28
David Sudarma <david@95west.co>
2015-06-02 11:26:04 -0700

    Disabled Mobile Scaling and Zooming

--- notedeck/includes/templates/notedeck_header.php
+++ notedeck/includes/templates/notedeck_header.php
@@ -7,5 +7,7 @@
 <link rel="stylesheet" href="<?php echo ND_95W_PLUGIN_URL . 'css/lightslider.min.css';?>" type="text/css" media="all">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"/>
 <link rel="stylesheet" href="<?php echo ND_95W_PLUGIN_URL . 'css/main.css';?>"/>
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 </head>
 <body>
