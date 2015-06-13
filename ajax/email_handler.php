c5bae7954aab289c45b944d719b8b5fc89ca3b92
Terrance Woest <terrance@tremli.com>
2015-06-13 14:24:19 -0700

    Fixed the email handler so that ajax requests are allowed to access the file directly.

--- ajax/email_handler.php
+++ ajax/email_handler.php
@@ -1,27 +1,34 @@
 <?php
-defined( 'ABSPATH' ) or die( 'Plugin file cannot be accessed directly.' );
+/* AJAX check  */
+if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
 
-require_once( reset(explode( 'wp-content', $_SERVER['SCRIPT_FILENAME'] )) . 'wp-load.php' );
+	// defined( 'ABSPATH' ) or die( 'Plugin file cannot be accessed directly.' );
 
-$email_message = $_POST['content'];
-$email_footer = '
+	require_once( reset(explode( 'wp-content', $_SERVER['SCRIPT_FILENAME'] )) . 'wp-load.php' );
+
+	$email_message = $_POST['content'];
+	$email_footer = '
 
 
---------------
+	--------------
 
-This message was sent via NoteDeck from ' . get_bloginfo( 'name' ) . '
-' . get_site_url();
+	This message was sent via NoteDeck from ' . get_bloginfo( 'name' ) . '
+	' . get_site_url();
 
-$email = $_POST['email'];
-$content = $email_message . $email_footer;
-$subject = get_field('notes_email_subject', 'option');
-$from = get_field('notes_email_from', 'option');
-$from = ($from == '') ? get_option('admin_email') : $from;
-$name = get_field('notes_email_name', 'option');
-$name = ($name == '') ? 'NoteDeck' : $name;
-$headers = 'From: ' . $name . ' <' . $from . '>' . "\r\n";
+	$email = $_POST['email'];
+	$content = $email_message . $email_footer;
+	$subject = get_field('notes_email_subject', 'option');
+	$from = get_field('notes_email_from', 'option');
+	$from = ($from == '') ? get_option('admin_email') : $from;
+	$name = get_field('notes_email_name', 'option');
+	$name = ($name == '') ? 'NoteDeck' : $name;
+	$headers = 'From: ' . $name . ' <' . $from . '>' . "\r\n";
 
-$check = wp_mail( $email, $subject, $content, $headers );
+	$check = wp_mail( $email, $subject, $content, $headers );
 
-echo $check;
+	die($check);
+} else {
+	die( 'Plugin file cannot be accessed directly.' );
+}
+
 ?>
